4137fe1f6a91ac98ca907c83e92bf56769e1c158
MatteV02 <veronimatteo1@gmail.com>
2025-05-26 13:24:26 +0200

    Edited and enhanced application to conform to integration test tests/integration_test.py

--- quiz_maker/main.py
+++ quiz_maker/main.py
@@ -1,29 +1,44 @@
 import sys
 from quiz_maker.question_extractor.JSONQuestionExtractor import JSONQuestionExtractor
 from quiz_maker.output_generator.HTMLOutputGenerator import HTMLOutputGenerator
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentError
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(
         prog='Quiz Maker',
-        description='make quiz',
-        epilog=''
+        description='Utility to make interactive quizes from questions.\nUsage guide at https://github.com/MatteV02/quiz_maker',
+        epilog='Realesed under Unlicense license by MatteV02 https://github.com/MatteV02',
+        exit_on_error=False
     )
-    parser.add_argument('-i', '--in-file')
-    parser.add_argument('-o', '--out-file')
-    args = parser.parse_args()
-
-    in_file = args.in_file
-    out_file = args.out_file
+    parser.add_argument('-i', '--in-file', help='Path to the input file; supported formats: JSON')
+    parser.add_argument('-o', '--out-file', help='Path to the output file; supported formats: HTML')
+    try:
+        args = parser.parse_args()
+        in_file = args.in_file
+        if in_file is None:
+            raise ValueError('Missing -i argument')
+        out_file = args.out_file
+        if out_file is None:
+            raise ValueError('Missing -o argument')
+    except:
+        print('quiz_maker: \tWrong usage, arguments -i|--in-file and -o|--out-file are required. No other flags are supported.\n\t\tRun with option -h for more details')
+        sys.exit(1)
 
     extractor = JSONQuestionExtractor()
     generator = HTMLOutputGenerator()
 
     try:
         generator.write(extractor.read(in_file), out_file)
-    except:
-        # print useful messages
+    except ValueError as value_error:
+        if value_error.args[0] == 'JSON file structure is not conforming to the one the application expects':
+            print('quiz_maker: \tWrong usage, Input file is not conforming to specifics.\n\t\tRun with option -h for more details')
+            sys.exit(3)
+        else:
+            print('quiz_maker: \tWrong usage, Input file type is not supported.\n\t\tRun with option -h for more details')
+            sys.exit(2)
+    except OSError:
+        print('quiz_maker: \tWrong usage, invalid output file path.\n\t\tRun with option -h for more details')
         sys.exit(1)
     
     sys.exit(0)
--- tests/integration_test.py
+++ tests/integration_test.py
@@ -2,21 +2,35 @@ import subprocess
 
 
 def check_return_code(command: str, expected: int):
-    result = subprocess.run(command.split(' '))
+    result = subprocess.run(
+        command.split(' '),
+        stdout=subprocess.DEVNULL
+    )
     assert result.returncode == expected, command
 
 if __name__ == '__main__':     
+    # wrong arguments
+    # 1. wrong input tag
+    check_return_code('python -m quiz_maker.main -j tests/resources/one_question.json -o tests/resources/one_question.html', 1)
+    # 2. wrong output tag
+    check_return_code('python -m quiz_maker.main -i tests/resources/one_question.json -w tests/resources/one_question.html', 1)
+    # 3. only input tag
+    check_return_code('python -m quiz_maker.main -i tests/resources/one_question.json', 1)
+    # 4. only output tag
+    check_return_code('python -m quiz_maker.main -o tests/resources/one_question.html', 1)
+    # 5. non existing input file
     check_return_code('python -m quiz_maker.main -i tests/resources/non-existing.json -o tests/resources/non-existing.html', 1)
-
+    # 6. non existing output path
     check_return_code('python -m quiz_maker.main -i tests/resources/twenty_questions.json -o wrong/path/file.json', 1)
-
-    check_return_code('python -m quiz_maker.main -i tests/resources/wrong_format.json -o tests/resources/wrong_format.html', 1)
-            
-    check_return_code('python -m quiz_maker.main -i tests/resources/not_conforming/wrong_answers_type.json -o tests/resources/not_conforming/wrong_answers_type.html', 1)
-        
-    check_return_code('python -m quiz_maker.main -i tests/resources/one_question.json -o tests/resources/one_question.html', 0)
-
-    check_return_code('python -m quiz_maker.main -i tests/resources/no_questions.json -o tests/resources/no_question.html', 0)
-        
+    # 7. unsupported format input file
+    check_return_code('python -m quiz_maker.main -i tests/resources/wrong_format.json -o tests/resources/wrong_format.html', 2)
+    # 8. not conforming input file   
+    check_return_code('python -m quiz_maker.main -i tests/resources/not_conforming/wrong_answers_type.json -o tests/resources/not_conforming/wrong_answers_type.html', 3)
+    
+    # correct arguments
+    # 1. one question json file
+    check_return_code('python -m quiz_maker.main -i tests/resources/one_question.json --out-file tests/resources/one_question.html', 0)
+    # 2. no questions json file
+    check_return_code('python -m quiz_maker.main --in-file tests/resources/no_questions.json -o tests/resources/no_question.html', 0)
+    # 3. twenty questions json file 
     check_return_code('python -m quiz_maker.main -i tests/resources/twenty_questions.json -o tests/resources/twenty_questions.html', 0)
-
